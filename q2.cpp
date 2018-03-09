f6c894de9b29b82b84026103c0049cb60051c188
Sudipta Biswas <31337864+SudiptaBiswas1804@users.noreply.github.com>
2018-03-10 01:04:09 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,135 +1,131 @@
-//Implementing binary search in linkedlist
-
+//Programme for binary search in linked list
 #include <iostream>
 using namespace std;
 
-struct node // structure having variable for data section and a pointer for the next node. 
-
-{
- int data;
- node *next;
- };	
- 
-   class linkedList //  class  containing the functions to handle the nodes
-{
- private:
-    node *head, *tail; //Declaring two important pointers, i.e. head and tail
- public:
-    linkedList() //The constructer will make them NULL to avoid any garbage value.
- {
-  head=NULL;
- tail=NULL;
-  }
- void insert(int value) // Function to enter values at the end of list
-    {
-      node *temp=new node;
-      temp->data=value;
- temp->next=NULL;
- if(head==NULL)  //When the list is empty
-     {
-       head=temp;
-      }
-    else
-        tail->next=temp;
-        tail=temp;
-    }
-
-   void display()// to display the elements of the list
-    {
-
-   	node*temp= new node;
-    	temp=head;
-    	cout<<" list is:\n";
-    	while(temp!=NULL)
-
-    	{
-    		cout<<temp->data<<"->";
-    		temp=temp->next;
-	}
-    cout<<"NULL"<<endl;
-   }
-
-   int sort(int n) //Arranging the list's element in ascending order using bubblesort
-
-{           node *p=new node;
-	node *q=new node;
-for (int i=0;i<n;i++)
-
-    {      p=head;
-		for (int j=0;j<(n-i)-1;j++)
-{
-
-   	if(p->data > p->next->data)   
-   	{
-
-   	q->data=p->data;           //swapping the values to arrange in proper order
-   	p->data=p->next->data;
-   	p->next->data=q->data;
-   		
-   	}
-   	p=p->next;
-   }
-    }
-    return 0;
-}
-//function to search the required element using binary search
-int binarySearch(int beg, int end,int num)
-{     
-         	while(beg<=end)//The loop will run till the element is found.
-	{
-	int mid=(beg+end)/2;
-	node *middle=new node; //creating a node middle
-	middle=head;
-
-		for(int i=0;i<mid;i++) //pointing the middle node to the mid element
-		{
-		  middle=middle->next;
-		}
-	if(middle->data==num)//To check if middle element is located at the middle of list
-	{
-		return mid+1;
-	}
-	else if(middle->data>num)
-	{
-		end=mid-1;
-	}
-	else if(middle->data<num)
-	{
-	            beg=mid+1;
-	           
-	}
-	}
-return -1;	//to return -1 when the element is not present in the loop.
-}
+struct node{
+	int data;
+	node *prev;
+	node *next;
 };
 
-int main() {
-	int n,num,el,beg=0;
- linkedList l; // Declaring object of class linkedList
- cout<<"\nEnter the number of elements in the list:";
- cin>>n;
- cout<<n;
- cout<<"\nEnter the elements of list in any order.";
- for(int i=0;i<n;i++)
- {   cin>>el;
- 	l.insert(el);
- }
- 
- cout<<"The unsorted ";
- l.display(); //calling display function to print the unsorted list
- l.sort(n);  //calling sort function to rearrange the array elements
- cout<<"\nThe sorted ";
- l.display();  //calling the display function to print the sorted array
- 
- cout<<"\nEnter the element to be searched:";
-cin>>num;
-cout<<num;
-int pos=l.binarySearch(0,n-1,num);
-if (pos==-1)
-{
-	cout<<"\nElement is not found.";
+class linkedlist{
+	private:
+	node *head;
+	node *tail;
+	public:
+	linkedlist(){
+	head=NULL;
+	tail=NULL;
+	}
+	
+	//Addition function - function for inserting a node at the end
+    void addnode ( int n){
+        node *temp=new node;
+        temp->data=n;
+        temp->next=NULL;
+        //If the linked list is empty 
+        if (head==NULL){
+            head=temp;
+            tail=temp;
+        }
+        //If the linked list is not empty
+        else
+        tail->next=temp;
+        tail=temp;
+        }
+
+    //Counting function - function for counting the number of elements in the linked list
+    int countnode(){
+        node *temp;
+        temp=head;
+        int length=0;
+        if (head==NULL){
+            cout<<"The list is empty"<<"\n";
+        }
+        else if(head!=NULL){
+            while(temp!=NULL) {
+        	    length++;
+                temp=temp->next;
+            }
+            cout<<"The number of elements in the linkedlist are : "<<length<<"\n";
+        }
+        return length;
+    }
+
+//Sorting function - function for bubble sorting the linked list
+    int bubblesort(int n){
+    	node *cur=new node;
+    	node *p=new node;
+    	for(int i=0; i<n; i++){
+    		p=head;
+    		for(int j=0; j<(n-i)-1; j++){
+    			if(p->data>p->next->data){
+    				cur->data=p->data;
+    				p->data=p->next->data;
+    				p->next->data=cur->data;
+    			}
+    			p=p->next;
+    		}
+    	}
+    	return 0;
+    }
+    
+    //Display function - function for dispalying the linked list
+    void display(){
+    	node *temp=new node;
+    	temp=head;
+    	if(head!=NULL){
+    		while(temp!=NULL){
+    			cout<<temp->data<<"->";
+    			temp=temp->next;
+    		}
+    		cout<<"NULL"<<endl;
+    	}
+    }
+    
+    //Searching function - function to search an node using binary search
+    int binarysearch (int left, int right, int n2){
+    	while(left<=right){
+    		int mid=(left+right)/2;
+    		node *middle=new node;
+    		middle=head;
+    		for(int i=0; i<mid; i++){
+    			middle=middle->next;
+    		}
+    		if(middle->data==n2){
+    			return mid+1;
+    		}
+    		else if(middle->data>n2){
+    			right=mid-1;
+    		}
+    		else if(middle->data<n2){
+    			left=mid+1;
+    		}
+    	}
+    	return -1;
+    }
+};
+
+int main(){
+	int n,num;
+	linkedlist l;
+	l.addnode(43);	
+	l.addnode(42);
+	l.addnode(56);
+	l.addnode(40);
+	l.addnode(54);
+	l.display();
+	n=l.countnode();
+	l.bubblesort(n);
+	l.display();
+	num=43;
+	int pos=l.binarysearch(0,n-1,num);
+            if (pos==-1)
+            {
+	   cout<<"Element is not found "<<endl;
+            }
+            else
+               cout<<"The element is found at "<<pos<<" th position "<<endl;
+    return 0;
 }
-else
-cout<<"\nThe element is found at "<<pos<<" th position. ";
-	return 0;
-}
+ 
